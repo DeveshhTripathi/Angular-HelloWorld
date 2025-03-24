292e154cfbacd3f7cb33c86424fd4c356ab5561b
DeveshhTripathi <deveshht591@gmail.com>
2025-03-24 14:36:02 +0530

    [Devesh Tripathi] add. BridgeLabz logo using property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  message= 'Hello from bridgelabz';
+  message: string = "Hello from BridgeLabz";
+  imgUrl = "https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM"
 }
+
